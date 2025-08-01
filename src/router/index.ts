1f5691eeedf47beb8960f9610be08ecf8703cfbe
袁家建 <112946977+bill-yuen@users.noreply.github.com>
2025-08-01 16:29:57 +0800

    feat(路由): 添加带有首页和登录视图的vue-router设置

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@views/home/index.vue'
+import Login from '@views/login/index.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@views': path.resolve(__dirname, 'src/views'),
+      },
+  },
 })
